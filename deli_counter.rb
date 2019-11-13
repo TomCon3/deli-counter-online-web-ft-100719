bafa0d1cd6eff3284e1bfec73f83a084b243bd81
Tommy Connor <tconnor98@aol.com>
2019-11-13 19:45:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+katz_deli = []
+
+another_line = katz_deli.join
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently #{another_line.each_with_index}"
+end 
+end
