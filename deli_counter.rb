42f59045755733ea05598923eff6ec7b952ef38b
Tommy Connor <tconnor98@aol.com>
2019-11-13 19:55:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def line(other_deli)
   if katz_deli.length == 0
-    
+    other_deli = []
     puts "The line is currently empty."
   else
     puts "The line is currently #{another_line.each_with_index}"
