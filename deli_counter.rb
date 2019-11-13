efc3e1d9a15c5824e2694b8b75f0417e68cb133c
Tommy Connor <tconnor98@aol.com>
2019-11-13 19:57:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(other_deli)
   if katz_deli.length >= 1
     other_deli = []
     counter = 1
-    katz_deli.each do 
+    katz_deli.each do |name|
     puts "The line is currently empty."
   else
     puts "The line is currently #{another_line.each_with_index}"
