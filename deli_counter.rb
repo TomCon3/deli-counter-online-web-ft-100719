083346e785bea75433d9600143379fa9d7362daa
Tommy Connor <tconnor98@aol.com>
2019-11-13 19:56:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,8 @@ katz_deli = []
 def line(other_deli)
   if katz_deli.length >= 1
     other_deli = []
-    counter = 1 
+    counter = 1
+    katz_deli.each do 
     puts "The line is currently empty."
   else
     puts "The line is currently #{another_line.each_with_index}"
